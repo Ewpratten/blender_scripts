8eaa4cc1b39477d04b37a7a71b8512378511f5f8
Evan Pratten <ewpratten@gmail.com>
2022-01-16 15:34:47 -0500

    Add what should be all of the logic

--- addons/eds/common/apps/updater/window.py
+++ addons/eds/common/apps/updater/window.py
@@ -46,6 +46,7 @@ class SelfUpdaterWindow(QtWidgets.QWidget):
         self.allow_dev_checkbox = QtWidgets.QCheckBox(
             "Allow development versions")
         self.allow_dev_checkbox.setChecked(False)
+        self.allow_dev_checkbox.stateChanged.connect(lambda state: self.redraw_versions_list())
         self.layout().addWidget(self.allow_dev_checkbox)
 
         # Add a list for all available versoins
@@ -68,21 +69,78 @@ class SelfUpdaterWindow(QtWidgets.QWidget):
 
     def redraw_versions_list(self):
         self.versions_list.clear()
-        logger.info(f"Module path is: {MODULE_PATH}")
 
-        # Call git to get a list of all local branches
-        git_output = os.popen("git branch").read()
+        # Navigate to the directory containing the plugin
+        curdir = os.getcwd()
+        os.chdir(MODULE_PATH)
+
+        # Call git to get a list of all local branches and tags
+        git_branches = os.popen("git branch").read()
+        git_tags = os.popen("git tag").read()
 
         # Parse the output to get the branch names
-        git_output = git_output.split("\n")
-        git_output = [x.strip() for x in git_output if x.strip()]
-        git_output = [x for x in git_output if not x.startswith("*")]
-        
+        git_branches = git_branches.split("\n")
+        git_branches = [x.strip() for x in git_branches if x.strip()]
+        current_branch = [x for x in git_branches if x.startswith("*")][0].replace("*", "").strip()
+        git_branches = [x for x in git_branches if not x.startswith("*")]
+        try:
+            git_branches.remove("list")
+        except ValueError:
+            pass
+
+        # Parse the output to get the tag names
+        git_tags = git_tags.split("\n")
+        git_tags = [x.strip() for x in git_tags if x.strip()]
+
+        # Add the current branch to the list and select it
+        current_branch_label = QtWidgets.QListWidgetItem(current_branch)
+        current_branch_label.setSelected(True)
+        self.versions_list.addItem(current_branch_label)
+        self.versions_list.setCurrentItem(current_branch_label)
+
+        # Add all tags to the list
+        for tag in git_tags:
+            tag_label = QtWidgets.QListWidgetItem(tag)
+            self.versions_list.addItem(tag_label)
+
+        # Check if we should be showing development versions
+        if self.allow_dev_checkbox.isChecked():
+            # Add all branches to the list
+            for branch in git_branches:
+                branch_label = QtWidgets.QListWidgetItem(branch)
+                self.versions_list.addItem(branch_label)
+
+        # Return to the original directory
+        os.chdir(curdir)
 
     def check_for_updates(self):
         logger.info("Checking for updates")
-        pass
+        
+        # Navigate to the directory containing the plugin
+        curdir = os.getcwd()
+        os.chdir(MODULE_PATH)
+
+        # Fetch from git
+        os.system("git fetch")
+        
+        # Return to the original directory
+        os.chdir(curdir)
 
     def update(self):
         logger.info("Update button pressed. Attempting to update.")
-        pass
+        
+        # Navigate to the directory containing the plugin
+        curdir = os.getcwd()
+        os.chdir(MODULE_PATH)
+
+        # Get the selected version
+        selected_version = self.versions_list.currentItem().text()
+
+        # Check out the selected version
+        os.system("git checkout " + selected_version)
+
+        # Pull the selected version
+        os.system("git pull")
+        
+        # Return to the original directory
+        os.chdir(curdir)
